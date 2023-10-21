6c887d05d37e0082a6ac3f5d02332c08b26f961f
Ivan Volodin <ivanwolodin@gmail.com>
2023-10-21 18:14:42 +0500

    Add: Loader class

--- postgres_to_es/ETL.py
+++ postgres_to_es/ETL.py
@@ -1,4 +1,4 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
 from constants import ES_INDEX
 from db_connections import open_postgres_connection
 
@@ -39,12 +39,12 @@ class ETL():
         self._last_sync_date = datetime.now()
         self.data_extractor_obj = self.DataExtractor()
         self.data_transformer = self.DataTransformer()
+        self.data_loader = self.DataLoader()
 
     def run(self):
-        data = self.data_extractor_obj.collect_data()
-        self.data_transformer.transform_data(data)
-        print(self.data_transformer.chunk)
-        self.data_transformer.clear_aux_data()
+        raw_data = self.data_extractor_obj.collect_data()
+        es_data = self.data_transformer.transform_data(raw_data)
+        self.data_loader.load_to_es(es_data)
 
     class DataExtractor():
         def __init__(self) -> None:
@@ -138,34 +138,11 @@ class ETL():
             return self._merge_data()
 
     class DataTransformer():
-        # данные для вставки в ES
-        # data = {
-        #     "id": "1",
-        #     "imdb_rating": 8.5,
-        #     "genre": ["драма", "криминал"],
-        #     "title": "Тёмный рыцарь",
-        #     "description": "Фильм о Бэтмене и Джокере",
-        #     "director": "Кристофер Нолан",
-        #     "actors_names": ["Кристиан Бэйл", "Хит Леджер"],
-        #     "writers_names": ["Джонатан Нолан", "Кристофер Нолан"],
-        #     "actors": [
-        #         {"id": "1", "name": "Кристиан Бэйл"},
-        #         {"id": "2", "name": "Хит Леджер"}
-        #     ],
-        #     "writers": [
-        #         {"id": "1", "name": "Джонатан Нолан"},
-        #         {"id": "2", "name": "Кристофер Нолан"}
-        #     ]
-        # }
-
-        # # вставка данных
-        # res = es.index(index="movies", body=data)
-
         def __init__(self) -> None:
-            self.clear_aux_data()
+            self._clear_aux_data()
 
-        def clear_aux_data(self):
-            self.aux_dict = defaultdict(lambda: {
+        def _clear_aux_data(self):
+            self._aux_dict = defaultdict(lambda: {
                 'imdb_rating': None,
                 'genre': None,
                 'title': None,
@@ -176,39 +153,39 @@ class ETL():
                 'writers': [],
                 'director': None
             })
-            self.chunk = []
 
         def transform_data(self, data: list) -> list:
             if not data:
                 return
 
             for row in data:
-                if self.aux_dict.get(row['fw_id']) is None:
-                    self.aux_dict[row['fw_id']]['imdb_rating'] = row['rating']
-                    self.aux_dict[row['fw_id']]['genre'] = row['type']
-                    self.aux_dict[row['fw_id']]['title'] = row['title']
-                    self.aux_dict[row['fw_id']]['description'] = row['description']
+                if self._aux_dict.get(row['fw_id']) is None:
+                    self._aux_dict[row['fw_id']]['imdb_rating'] = row['rating']
+                    self._aux_dict[row['fw_id']]['genre'] = row['type']
+                    self._aux_dict[row['fw_id']]['title'] = row['title']
+                    self._aux_dict[row['fw_id']]['description'] = row['description']
 
-                if row['role'] == 'actor' and row['full_name'] not in self.aux_dict[row['fw_id']]['actors_names']:
-                    self.aux_dict[row['fw_id']]['actors_names'].append(row['full_name'])
-                    self.aux_dict[row['fw_id']]['actors'].append(
+                if row['role'] == 'actor' and row['full_name'] not in self._aux_dict[row['fw_id']]['actors_names']:
+                    self._aux_dict[row['fw_id']]['actors_names'].append(row['full_name'])
+                    self._aux_dict[row['fw_id']]['actors'].append(
                         {
                             'id': row['id'],
                             'name': row['full_name']
                         }
                     )
-                if row['role'] == 'writer' and row['full_name'] not in self.aux_dict[row['fw_id']]['writers_names']:
-                    self.aux_dict[row['fw_id']]['writers_names'].append(row['full_name'])
-                    self.aux_dict[row['fw_id']]['writers'].append(
+                if row['role'] == 'writer' and row['full_name'] not in self._aux_dict[row['fw_id']]['writers_names']:
+                    self._aux_dict[row['fw_id']]['writers_names'].append(row['full_name'])
+                    self._aux_dict[row['fw_id']]['writers'].append(
                         {
                             'id': row['id'],
                             'name': row['full_name']
                         }
                     )
-                if row['role'] == 'director' and row['full_name'] != self.aux_dict[row['fw_id']]['director']:
-                    self.aux_dict[row['fw_id']]['director'] = row['full_name']
+                if row['role'] == 'director' and row['full_name'] != self._aux_dict[row['fw_id']]['director']:
+                    self._aux_dict[row['fw_id']]['director'] = row['full_name']
 
-            for k, v in self.aux_dict.items():
+            chunk = []
+            for k, v in self._aux_dict.items():
                 filmwork = {
                     "id": k,
                     "imdb_rating": v['imdb_rating'],
@@ -222,7 +199,23 @@ class ETL():
                     "actors": v['actors'],
                     "writers": v['writers'],
                 }
-                self.chunk.append(filmwork)
+                chunk.append(filmwork)
+
+            self._clear_aux_data()
+
+            return chunk
 
     class DataLoader():
-        pass
+        def load_to_es(self, data):
+            if not data:
+                return
+            actions = [
+                {
+                    '_index': 'movies',
+                    '_id': row['id'],
+                    '_source': row,
+                }
+                for row in data
+            ]
+            res = helpers.bulk(es, actions)
+            print(res)
